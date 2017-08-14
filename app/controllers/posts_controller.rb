17189102c08aa1b5261d486dc7ce337311149306
Colton Mathews <mathews.colton@gmail.com>
2017-08-14 12:21:04 -0600

    Implimented CRUD for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
 class PostsController < ApplicationController
 
-def index
-end
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(params.require(:post).permit(:date, :rationale))
+        @post.save
+        
+        redirect_to(@post, :notice => "Thank you. You good lookin person you.")
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
 
 end
