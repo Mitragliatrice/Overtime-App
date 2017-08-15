380e03021fef30f24b72fb7ff8f1ec5dbe980381
Colton Mathews <mathews.colton@gmail.com>
2017-08-15 14:51:23 -0600

    implimented edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show]
+    before_action :set_post, only: [:show, :edit, :update]
 
     def index
         @posts = Post.all
@@ -9,25 +9,36 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
-    def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user.id
-        if @post.save
-            redirect_to @post, :notice => "Thank you. You good lookin person you."
-        else 
-            render :new
-        end
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to @post, :notice => "Thank you. You good lookin person you."
+    else 
+      render :new
     end
+  end
 
-    def show
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, :notice => "Thank you. You good lookin person you."
+    else
+      render :edit
     end
+  end
 
-    private
-        def post_params
-            (params.require(:post).permit(:date, :rationale))
-        end
+  def show
+  end
 
-        def set_post
-            @post = Post.find(params[:id])
-        end
+  private
+  def post_params
+    (params.require(:post).permit(:date, :rationale))
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
