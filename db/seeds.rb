99110e9c41149dc229af87704dff12338110a03c
Colton Mathews <mathews.colton@gmail.com>
2017-08-16 15:37:56 -0600

    implimented status enum on posts for approval workflow

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 
-1.times do |user|
-    User.create!(email: "tester@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Test", last_name: "User")
-end
-
-puts "Created 1 test user"
-
 1.times do |adminuser| 
     AdminUser.create!(email: "mathews.colton@gmail.com", password: "Colton11!", password_confirmation: "Colton11!", first_name: "Jon", last_name: "Snow")
 end
 
 puts "Created 1 test admin user"
+
+1.times do |user|
+    User.create!(email: "tester@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Test", last_name: "User")
+end
+
+puts "Created 1 test user"
 
 100.times do |post| 
     Post.create!(date: Date.today, rationale: "Test rationale number #{post}", user: User.last)
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,12 +2,18 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "Some Rationale"
+    status "pending"
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
+    status "pending"
     user
   end
+
+
+
+
 end
