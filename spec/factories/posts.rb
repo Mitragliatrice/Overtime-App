3a1775f0f7d53dab1d4aacf9df9840ffed6c9be0
Colton Mathews <mathews.colton@gmail.com>
2017-08-17 22:48:12 -0600

    implimented custom scope by users.

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,14 +2,12 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "Some Rationale"
-    status "pending"
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
-    status "pending"
     user
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,11 +25,24 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
     end
+
+    it 'has a scope so that only post creators can see their posts' do
+      post1 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "asdf", user_id: @user.id)
+
+      other_user = User.create(first_name: 'Non', last_name: 'Authorized', email:               "nonauth@example.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be   seen", user_id: other_user.id)
+
+      visit posts_path
+
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
   end
 
   describe 'new' do
-      it 'has a link from the homepage' do
+    it 'has a link from the homepage' do
       visit root_path
+
       click_link("new_post_from_nav")
       expect(page.status_code).to eq(200)
     end
@@ -38,6 +51,8 @@ describe 'navigate' do
   describe 'delete' do
     it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      # TODO refactor
+      @post.update(user_id: @user.id)
       visit posts_path
 
       click_link("delete_post_#{@post.id}_from_index")
@@ -71,7 +86,7 @@ describe 'navigate' do
     end
   end
 
-    describe 'edit' do
+  describe 'edit' do
     before do
       @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
       login_as(@edit_user, :scope => :user)
