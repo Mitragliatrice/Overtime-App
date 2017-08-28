4898f69ea9a6a7f0f7bfe9057138187ce94cb2e1
Colton Mathews <mathews.colton@gmail.com>
2017-08-28 16:05:08 -0600

    Updated Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,18 +21,17 @@ end
 
 puts "Created 1 test user"
 
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 13.days))
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 20.days))
+  
+puts "Created 3 audit logs"
+
 15.times do |post| 
     Post.create!(date: Date.today, rationale: "Amet quis labore incididunt labore. #{post} ", user: User.last, overtime_request: 2.5)
 end
 
 puts "Created 15 test posts"
 
-
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 13.days))
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 20.days))
-
-
-puts "Created 3 audit logs"
 
 
